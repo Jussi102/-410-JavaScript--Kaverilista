12ea120ae0105aad3d9021257b05dd75c3d2d30f
Jussi <Varonenjussi@gmail.com>
2026-09-22 08:49:39 +0300

    tehtävä valmis.

--- script.js
+++ script.js
@@ -8,28 +8,48 @@ function lisaa_nimi(event) {
     event.preventDefault();
 
     const nimi = document.getElementById("tekstikentta").value;
+
+    document.querySelector(".item-list").innerHTML += "<li>" + nimi + "</li>";
+
     kaverit.push(nimi);
 
 
 }
 
-
-
 function poista_nimi(event) {
     event.preventDefault();
 
-
     const nimi = document.getElementById("tekstikentta").value;
-    for (let i=0; i < kaverit.length; i++)
 
-        kaverit.indexOf(nimi);
+    const indeksi = kaverit.indexOf(nimi);
 
-    console.log(kaverit);
-    
+    if (indeksi !== -1) {
+        kaverit.splice(indeksi, 1);
+
+        document.querySelector(".item-list").innerHTML = "";
+
+        for (let i = 0; i < kaverit.length; i++) {
+            document.querySelector(".item-list").innerHTML += "<li>" + kaverit[i] + "</li>";
+        }
+    }
 }
 
 
+function jarjesta_nimi(event) {
+    event.preventDefault();
 
-document.querySelector("form").addEventListener("submit", lisaa_nimi);
 
-document.querySelector("form").addEventListener("submit", poista_nimi);
+    kaverit.sort();
+
+    document.querySelector(".item-list").innerHTML = "";
+
+    for (let i = 0; i < kaverit.length; i++) {
+        document.querySelector(".item-list").innerHTML += "<li>" + kaverit[i] + "</li>";
+    }
+
+
+}
+
+document.querySelector("#lisaa-kaveri").addEventListener("click", lisaa_nimi);
+document.querySelector("#poista-kaveri").addEventListener("click", poista_nimi);
+document.querySelector("#jarjesta-kaverit").addEventListener("click", jarjesta_nimi);
